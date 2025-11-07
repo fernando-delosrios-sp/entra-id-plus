f2e298d46c99bc1d6b74f1fec8b6bae4430cc660
Fernando de los Ríos Sánchez <fernando.delosrios@sailpoint.com>
2025-11-07 11:44:10 +0100

    Enhance getSponsors function to handle missing attributes and improve logging; ensure account attributes are checked before accessing them in setAccountAttribute.

--- src/operations.ts
+++ src/operations.ts
@@ -5,7 +5,7 @@ import { Config } from './model/config'
 import { getLogger } from './utils'
 
 export const setAccountAttribute = (account: AccountObject, attribute: string, value: any) => {
-    if (attribute.startsWith('attributes.')) {
+    if (attribute.startsWith('attributes.') && account.attributes) {
         account.attributes[attribute.substring(11)] = value
     } else {
         ;(account as any)[attribute] = value
@@ -15,21 +15,26 @@ export const setAccountAttribute = (account: AccountObject, attribute: string, v
 export const getSponsors: AccountOperation = async (account) => {
     const config: Config = await readConfig()
     const logger = getLogger(config.spConnDebugLoggingEnabled)
-    logger.debug(`Getting sponsors for ${account.attributes.userPrincipalName}`)
-    logger.debug('Loading client')
-    const client = new EntraIdClient(config.domainName, config.clientID, config.clientSecret)
-    logger.debug('Getting sponsors')
-    const sponsors = await client.getSponsorsForGuest(account.attributes.objectId as string)
-    logger.debug('Got sponsors')
-    if (sponsors.length === 0) {
-        logger.debug('No sponsors found')
-        return undefined
-    } else if (sponsors.length === 1) {
-        logger.debug('Found one sponsor: ' + sponsors[0].userPrincipalName)
-        return sponsors[0].userPrincipalName
+    if (account.attributes) {
+        logger.debug(`Getting sponsors for ${account.attributes.userPrincipalName}`)
+        logger.debug('Loading client')
+        const client = new EntraIdClient(config.domainName, config.clientID, config.clientSecret)
+        logger.debug('Getting sponsors')
+        const sponsors = await client.getSponsorsForGuest(account.attributes.objectId as string)
+        logger.debug('Got sponsors')
+        if (sponsors.length === 0) {
+            logger.debug('No sponsors found')
+            return undefined
+        } else if (sponsors.length === 1) {
+            logger.debug('Found one sponsor: ' + sponsors[0].userPrincipalName)
+            return sponsors[0].userPrincipalName
+        } else {
+            logger.debug('Found more than one sponsor: ' + sponsors.map((x: any) => x.userPrincipalName).join(', '))
+            return sponsors.map((x: any) => x.userPrincipalName)
+        }
     } else {
-        logger.debug('Found more than one sponsor: ' + sponsors.map((x: any) => x.userPrincipalName).join(', '))
-        return sponsors.map((x: any) => x.userPrincipalName)
+        logger.debug('No attributes found for ' + account.uuid)
+        return undefined
     }
 }
 
